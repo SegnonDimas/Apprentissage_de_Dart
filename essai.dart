4a06beba5390601e871071efc045b5e7e3cdabfb
SegnonDimas <segnondimas@gmail.com>
2024-12-04 11:58:27 +0100

    Apprentissage de quelques types de variables en Dart

--- essai.dart
+++ essai.dart
@@ -1,1 +1,25 @@
-void main() {}
+void main() {
+  //Variables
+  /*
+  var : pour la déclaration d'une variable quelconque
+  int : Entier
+  float : Nombre à virgule flottante
+  char : Caractère
+  bool : Vrai ou Faux
+  String : Chaines de caractères
+  List : liste de variables de même type
+  */
+
+  /* APPRENTISSAGE DE LA DÉCLARATION DE VARIABLES*/
+  //déclaration de la variable nom dont la valeur initiale est Bob;
+  var nom = "Bob";
+
+  // variable age de type entier
+  int age = 12;
+
+  //afficher la valeur de la variable nom
+  print(nom);
+
+  // afficher le type de ma variable nom
+  print(nom.runtimeType);
+}
